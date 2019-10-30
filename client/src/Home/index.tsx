822bffefdba1cbb3be53f0a51c8626369c8a50db
Mark Washington <thatbrothacodes@gmail.com>
2019-10-30 00:20:08 -0600

    added how to on home page

--- client/src/Home/index.tsx
+++ client/src/Home/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 export default function Home() {
     return(
-        <h1>Home</h1>
+        <>
+            <Typography variant="h2">
+                How To
+            </Typography>
+            <br />
+            <br />
+            <Typography variant="h4">
+                Getting Contact Details
+            </Typography>
+            <Typography variant="body1">
+                Select an item from the left side menu
+            </Typography>
+            <br />
+            <br />
+            <Typography variant="h4">
+                Searching for a Contact
+            </Typography>
+            <Typography variant="body1">
+                Enter 3 letters or more to search for a particular person
+            </Typography>
+        </>
     )
 }
